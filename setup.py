ce67050c598a66543d519240b58c7e834931bcd0
PaperMtn <andrewbyford86@hotmail.com>
2020-01-09 21:23:37 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lil-pwny',
+    version='1.0.0',
+    packages=['src'],
+    url='https://github.com/PaperMtn/little-pwny',
+    license='GPL-3.0',
+    author='PaperMtn',
+    author_email='',
+    long_description='A multiprocessing approach to auditing Active Directory passwords against HIBP using Python.',
+    description='Auditing Active Directory Passwords '
+)
